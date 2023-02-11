ebecfc8c86c1c432468e308ca576dbfd816b1c57
UlyanaQA <mozgulya@gmail.com>
2023-02-11 23:57:26 +0300

    Скрипт работает - 3я попытка, удалены лишние закомментированные строки кода

--- TestAddGroup.py
+++ TestAddGroup.py
@@ -12,16 +12,12 @@ class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-        # self.base_url = "https://www.google.com/"
-        # self.verificationErrors = []
-        # self.accept_next_alert = True
 
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element(By.NAME, "user").clear()
         wd.find_element(By.NAME, "user").send_keys("admin")
-        #wd.find_element_by_name("pass").click()
         wd.find_element(By.NAME, "pass").clear()
         wd.find_element(By.NAME, "pass").send_keys("secret")
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
@@ -37,6 +33,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys("test grioup1 footer")
         wd.find_element(By.NAME, "submit").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
         wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def is_element_present(self, how, what):
@@ -52,18 +49,6 @@ class TestAddGroup(unittest.TestCase):
         except NoAlertPresentException as e:
             return False
         return True
-
-    '''def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True'''
 
     def tearDown(self):
         self.wd.quit()
