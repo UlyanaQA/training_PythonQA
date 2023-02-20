d87a1d6ed2fee085b3921ed35ca4084c78ed9c1e
UlyanaQA <mozgulya@gmail.com>
2023-02-20 23:23:41 +0300

    Занятие 2.3: выделен класс SessionHelper для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -7,11 +8,7 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-        wd.find_element(By.NAME, "user")
+        self.session = SessionHelper(self)
 
     def return_to_groups_page(self):
         wd = self.wd
@@ -39,15 +36,6 @@ class Application:
     def open_groups_page(self):
         wd = self.wd
         wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def open_home_page(self):
         wd = self.wd
