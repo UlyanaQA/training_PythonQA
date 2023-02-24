faac9525982fd66de8e2d72f38a6fef6a628e705
UlyanaQA <mozgulya@gmail.com>
2023-02-24 11:06:10 +0300

    Занятие 2.3: созданы пакеты для разных зон ответсвтенности

--- test/TestAddContact.py
+++ test/TestAddContact.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Test add contact. One fixture
 import pytest
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 
 
 @pytest.fixture
