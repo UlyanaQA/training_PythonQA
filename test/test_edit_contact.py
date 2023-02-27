b7de92b7cd9756b0c1cfc8866a7a1a761fecfb89
UlyanaQA <mozgulya@gmail.com>
2023-02-27 22:27:05 +0300

    Занятие 3.4: коммит перед занятием 3.5 (после Задания №8)

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,14 +1,99 @@
+import time
+
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(
-        Contact(firstname="EDIT_contact55", middlename="Edit_middle", lastname="Edit_last", nickname="Edit_Nick",
-                title="Edit_title", company="ED_SpaceX",
-                address="The Mars", phone1="111110", mobilephone="222220", workphone="33330", fax="44440",
-                email1="editmozgulya@gmail.com",
-                email2="edittest@ght.ru", email3="edittest@kil.com", site="space.com", bday="7", bmonth="April",
-                byear="1998", aday="8", amonth="May", ayear="2024",
-                address2="The Mars", phone2="55555", notes="Some changes"))
-    app.session.logout()
+def test_edit_firstname(app):
+    app.contact.edit_first_contact(Contact(firstname="100Edit_first"))
+
+
+def test_edit_middlename(app):
+    app.contact.edit_first_contact(Contact(middlename="100Edit_middle"))
+
+
+def test_edit_lastname(app):
+    app.contact.edit_first_contact(Contact(lastname="100Edit_last"))
+
+
+def test_edit_nickname(app):
+    app.contact.edit_first_contact(Contact(nickname="1Edit_Nick"))
+
+
+def test_edit_title(app):
+    app.contact.edit_first_contact(Contact(title="1Edit_title"))
+
+
+def test_edit_company(app):
+    app.contact.edit_first_contact(Contact(company="1ED_SpaceX"))
+
+
+def test_edit_address(app):
+    app.contact.edit_first_contact(Contact(address="1The Mars"))
+
+
+def test_edit_phone1(app):
+    app.contact.edit_first_contact(Contact(phone1="011111"))
+
+
+def test_edit_mobilephone(app):
+    app.contact.edit_first_contact(Contact(mobilephone="022222"))
+
+
+def test_edit_workphone(app):
+    app.contact.edit_first_contact(Contact(workphone="03333"))
+
+
+def test_edit_fax(app):
+    app.contact.edit_first_contact(Contact(fax="04444"))
+
+
+def test_edit_email1(app):
+    app.contact.edit_first_contact(Contact(email1="1editmozgulya@gmail.com"))
+
+
+def test_edit_email2(app):
+    app.contact.edit_first_contact(Contact(email2="1edittest@ght.ru"))
+
+
+def test_edit_email3(app):
+    app.contact.edit_first_contact(Contact(email3="1edittest@kil.com"))
+
+
+def test_edit_site(app):
+    app.contact.edit_first_contact(Contact(site="1space.com"))
+
+
+def test_edit_bday(app):
+    app.contact.edit_first_contact(Contact(bday="18"))
+
+
+def test_edit_bmonth(app):
+    app.contact.edit_first_contact(Contact(bmonth="May"))
+
+
+def test_edit_byear(app):
+    app.contact.edit_first_contact(Contact(byear="1996"))
+
+
+def test_edit_aday(app):
+    app.contact.edit_first_contact(Contact(aday="5"))
+
+
+def test_edit_amonth(app):
+    app.contact.edit_first_contact(Contact(amonth="April"))
+
+
+def test_edit_ayear(app):
+    app.contact.edit_first_contact(Contact(ayear="2022"))
+
+
+def test_edit_address2(app):
+    app.contact.edit_first_contact(Contact(address2="1The Mars"))
+
+
+def test_edit_phone2(app):
+    app.contact.edit_first_contact(Contact(phone2="055555"))
+
+
+def test_edit_notes(app):
+    app.contact.edit_first_contact(Contact(notes="1Some changes"))
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,9 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="group_edit", header="edit group header", footer="edit group footer"))
-    app.session.logout()
+def test_edit_group_name(app):
+    app.group.edit_first_group(Group(name="new group"))
+
+
+def test_edit_group_header(app):
+    app.group.edit_first_group(Group(header="new header"))
