fc29b3c003938acd58ad9e14233c72981495bd18
UlyanaQA <mozgulya@gmail.com>
2023-03-05 13:50:15 +0300

    Занятие 4.1: реализована загрузка информации о группах в виде списков

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,10 +4,16 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="old group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="new group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="old header"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="new header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
