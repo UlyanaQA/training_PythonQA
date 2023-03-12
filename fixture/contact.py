aa4ca43fffb84c0d13ef66fa7877264f15fa70bd
UlyanaQA <mozgulya@gmail.com>
2023-03-12 18:13:59 +0300

    Задание №14: реализованы проверки для всех полей контакта на главной странице

--- fixture/contact.py
+++ fixture/contact.py
@@ -114,16 +114,22 @@ class ContactHelper:
     def get_contact_info_from_edit_page(self, index):
         wd = self.app.wd
         self.open_contact_to_edit_by_index(index)
+        id = wd.find_element(By.NAME, "id").get_attribute("value")
         firstname = wd.find_element(By.NAME, "firstname").get_attribute("value")
         lasttname = wd.find_element(By.NAME, "lastname").get_attribute("value")
-        id = wd.find_element(By.NAME, "id").get_attribute("value")
+        middlename = wd.find_element(By.NAME, "middlename").get_attribute("value")
+        address = wd.find_element(By.NAME, "address").get_attribute("value")
         homephone = wd.find_element(By.NAME, "home").get_attribute("value")
         workphone = wd.find_element(By.NAME, "work").get_attribute("value")
         mobilephone = wd.find_element(By.NAME, "mobile").get_attribute("value")
         secondaryphone = wd.find_element(By.NAME, "phone2").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lasttname, id=id,
-                       homephone=homephone, mobilephone=mobilephone, workphone=workphone,
-                       secondaryphone=secondaryphone)
+        email1 = wd.find_element(By.NAME, "email").get_attribute("value")
+        email2 = wd.find_element(By.NAME, "email2").get_attribute("value")
+        email3 = wd.find_element(By.NAME, "email3").get_attribute("value")
+
+        return Contact(id=id, firstname=firstname, middlename=middlename, lastname=lasttname,
+                       address=address, homephone=homephone, mobilephone=mobilephone, workphone=workphone,
+                       secondaryphone=secondaryphone, email1=email1, email2=email2, email3=email3)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
@@ -154,11 +160,14 @@ class ContactHelper:
             self.open_contact_list()
             self.contact_cache = []
             for element in wd.find_elements(By.CSS_SELECTOR, "[name=entry]"):
+                id = element.find_element(By.NAME, "selected[]").get_attribute("value")
                 cells = element.find_elements(By.TAG_NAME, "td")
                 lastname = element.find_element(By.XPATH, "./td[2]").text
                 firstname = element.find_element(By.XPATH, "./td[3]").text
-                id = element.find_element(By.NAME, "selected[]").get_attribute("value")
+                address = element.find_element(By.XPATH, "./td[4]").text
+                all_emails = cells[4].text
                 all_phones = cells[5].text
-                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id,
+                self.contact_cache.append(Contact(id=id, lastname=lastname, firstname=firstname,
+                                                  address=address, all_emails_from_home_page=all_emails,
                                                   all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
