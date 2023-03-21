c8f6d39f7042a0ee4b78edaff25371c701987c79
UlyanaQA <mozgulya@gmail.com>
2023-03-21 22:27:21 +0300

    Задание №18-19: реализованы генераторы тестовых данных для групп и контактов, сохраняющие созданные данные в файл в формате JSON. Реализована загрузка тестовых данных из файлов в формате JSON и параметризация тестов путём "внедрения" этих тестовых данных в фикстуру при помощи hook-функции pytest_generate_tests.

--- conftest.py
+++ conftest.py
@@ -38,6 +38,7 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--target", action="store", default="target.json")
 
+
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
         if fixture.startswith("data_"):
@@ -47,8 +48,10 @@ def pytest_generate_tests(metafunc):
             testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
+
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
 
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
